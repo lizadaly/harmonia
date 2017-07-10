636822442202561e96e75b352a59be974144d3f7
Liza Daly <lizadaly@gmail.com>
2017-07-10 16:15:18 -0400

    [#2] Change text in C1 to reflect no computer

--- chapters/chapter01.js
+++ chapters/chapter01.js
@@ -14,7 +14,7 @@ export default ({currentSection, inventory, cards}) => {
         miles of solo driving. The days had gotten shorter and colder and lonelier the further
         I went from home. <ListCard expansions={["St. Isidore College"]}
           tag="c1-college-name"
-          card={<span>A tiny Catholic college, tucked in the upper left corner of
+          card={<span>A tiny Catholic college, tucked in the upper corner of
           Massachusetts like a bookmark.</span>} /> would be my home now.
       </p>
       <p>
@@ -49,53 +49,22 @@ export default ({currentSection, inventory, cards}) => {
           Lynn's area of scholarship was 19th century mysticism. Mine was Civil War-era pre-feminist
           movements. Close enough.
         </span>}
-      />
-    </p>
-    <p>
-      His door was closed but not locked, and curling up under the doorframe
-      was <List expansions={[["a glowing light"], ["an unnatural glow, a color out of nature"]]}
-        tag="c1-glow"
-          />.
-
+      />. His door was closed but <List expansions={[["not locked"], ["not locked, so I let myself in"]]} tag="c1-in" />.
     </p>
   </section>,
   <section>
     <p>
-      I opened the door.  Lynn's office was small, stuffed with an absolute riot of stacked papers, notes, and library books.
+      Lynn's office was small, stuffed with an absolute riot of stacked papers, notes, and library books.
       It had the feel of having been abandoned by its owner.
     </p>
     <p>
-      Incongruously, there was a pretty advanced-looking  <ListCard expansions={["computer terminal"]}
-        tag="c1_tty"
-        nextUnit={null}
-        card={<span>
-          I didn't own a computer, but I used the ones at the university library a lot. This one was pretty
-          different though.
-        </span>}
-      /> in the center of the room, with a desk all to itself.
-      It had a <List expansions={[["brightly lit screen"], ["brightly lit screen"]]} tag="c1_screen" /> and
-      no keyboard, just a <List expansions={[["small box"], ["small box topped with a round button"]]}
-        tag="c1_mouse"
-        nextUnit={null} />, tethered to a cord. Thicker cables snaked out of the back of the machine and
-      disappeared into a <ListCard expansions={["hastily-punched hole"]}
-        tag="c1-hole"
-        card={<span>I'd learn later that these led directly to the new computer lab.</span>} /> in the wall by the floor.
-    </p>
-  </section>,
-  <section>
-    <Map from={inventory.c1_mouse} to={{
-      "_undefined": <p>It was, at first, just a blank amber screen.</p>,
-      "_any": <div>
-        <p>Curious, I picked up the box; there was a wheel underneath. I turned it back over in my hand and
-          pressed the button. The
-          computer emitted a <ListCard expansions={["harsh loud beep"]} tag="c1-beep" card={<span>Okay, I jumped.</span>} />.
-          The display wiped slowly from top to bottom and
-          a <List expansions={[["complex interface"], ["complex interface"]]}
-              tag="c1-display" nextUnit="chapter" /> appeared in its place:</p>
-      </div>
+      His desk was a disaster, but a small worktable by the doorway had just a single stack of papers
+      with my class's syllabus on top. I set it aside and <ListCard expansions={["started to flip through the rest:"]}
+        tag="c1-pile"
+        card={<span>Look if he didn't want his stuff read, he shouldn't have gone AWOL.</span>} />
 
-    }}
-  />
+    </p>
+
   </section>
 
   ]
