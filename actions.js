27d4a964b174d7169bd3e4df4c15130f43539be1
Liza Daly <lizadaly@gmail.com>
2017-09-16 08:59:09 -0400

    Remove lines

--- actions.js
+++ actions.js
@@ -1,16 +1,8 @@
 export const CARD_CREATED = "CARD_CREATED"
-export const LINE_CREATED = "LINE_CREATED"
 
 export const cardCreated = (tag) => {
   return {
     type: CARD_CREATED,
-    tag
-  }
-}
-
-export const lineCreated = (tag) => {
-  return {
-    type: LINE_CREATED,
     tag
   }
 }
